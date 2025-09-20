c027f4f6759fafc94694680f3fda6c647af5f23d
Ruel9 <univcos91@gmail.com>
2025-09-21 00:01:31 +0900

    feat: params 수정

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -4,8 +4,9 @@
 
 
 export default async function Read({params}){
-
-    const resp = await fetch(`http://localhost:9999/topics/${params.id}`);
+    /*
+    const {id} = await params;
+    const resp = await fetch(`http://localhost:9999/topics/${id}`);
     const topic = await resp.json();
   
 
@@ -17,4 +18,27 @@ export default async function Read({params}){
             
         </>
     )
+        */
+     try {
+        const {id} = await params;
+        
+        const resp = await fetch(`http://localhost:9999/topics/${id}`);
+        if (!resp.ok) {
+            throw new Error(`HTTP error! status: ${resp.status}`);
+        }
+        
+        const topic = await resp.json();
+        
+        return (
+            <>
+                <h2>{topic.title}</h2>
+                {topic.body}
+            </>
+        );
+    }
+     catch (error) {
+        console.error('데이터를 가져오는 중 오류 발생:', error);
+        return <div>오류가 발생했습니다.</div>;
+    }
+   
 }
